9c5c77f2fd168d25b06e12145feb029d822cd4d5
jisnotu <133517588+jisnotu@users.noreply.github.com>
2024-12-02 19:14:00 -0500

    Write pseudocode for getComputerChoice function

---
+++ javascript.js
@@ -0,0 +1,5 @@
+// PSEUDOCODE for function `getComputerChoice()`:
+// Generate a random number x within a range.
+// If x is within the first third of the range, return rock.
+// If x is within the second third of the range, return paper.
+// If x is within the last third of hte range, return scissors.
