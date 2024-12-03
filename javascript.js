3cad9d19fdd99ba1a419d4c923b0060ba101ea73
jisnotu <133517588+jisnotu@users.noreply.github.com>
2024-12-03 17:25:43 -0500

    Add function to play a round

--- javascript.js
+++ javascript.js
@@ -29,3 +29,73 @@ function getHumanChoice() {
 
 let humanScore = 0;
 let computerScore = 0;
+
+// PSEUDOCODE for function `playRound()`:
+// Given the human's choice and the computer's choice...
+function playRound(humanChoice, computerChoice) {
+  // Convert human's choice to all lowercase.
+  humanChoice = humanChoice.toLowerCase();
+
+  // Verify if the human's choice is a valid choice.
+  switch (humanChoice) {
+    // If it's valid, keep going.
+    case "rock":
+    case "paper":
+    case "scissors":
+      break;
+    // If it's not valid, then write a message in the console that says so and stop.
+    default:
+      console.log(`Invalid choice; no winner.`);
+      return;
+  }
+  
+  // Compare the human's choice to the computer's choice.
+  // If they're equal, write a tie message in the console and don't increment either score.
+  if (humanChoice === computerChoice) {
+    console.log(`You both chose ${computerChoice}. It's a tie!`);
+    return;
+  }
+  // If they're different, write whether the human won or lost and increment the appropriate player's score.
+  if (humanChoice === "rock") {
+    switch (computerChoice) {
+      case "paper":
+        console.log(`Rock loses to paper. You lost!`);
+        computerScore++;
+        break;
+      case "scissors":
+        console.log(`Rock beats scissors. You won!`)
+        humanScore++;
+        break;
+      default:
+        console.log(`Uh oh. You're not supposed to reach this part of the code.`);
+    }
+  }
+  else if (humanChoice === "paper") {
+    switch (computerChoice) {
+      case "rock":
+        console.log(`Paper beats rock. You won!`);
+        humanScore++;
+        break;
+      case "scissors":
+        console.log(`Paper loses to scissors. You lost!`)
+        computerScore++;
+        break;
+      default:
+        console.log(`Uh oh. You're not supposed to reach this part of the code.`);
+    }
+  }
+  else { // humanChoice === "scissors"
+    switch (computerChoice) {
+      case "rock":
+        console.log(`Scissors loses to rock. You lost!`);
+        computerScore++;
+        break;
+      case "paper":
+        console.log(`Scissors beats paper. You won!`)
+        humanScore++;
+        break;
+      default:
+        console.log(`Uh oh. You're not supposed to reach this part of the code.`);
+    }
+  }
+}
